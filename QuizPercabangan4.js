4ab3d2ee6277974972b2ff2884eaccd24a66bc3e
DI-One1 <mochcomeback@gmail.com>
2025-01-30 10:16:45 +0700

    Create QuizPercabangan4.js

---
+++ QuizPercabangan4.js
@@ -0,0 +1,9 @@
+// Memasukkan nilai secara langsung
+let nilaiDdpk = 100;
+
+// Menentukan apakah lulus atau tidak
+if (nilaiDdpk > 75) {
+    console.log("Selamat Anda Lulus!");
+} else {
+    console.log("Mohon Maaf Tidak Lulus, Jangan Menyerah!");
+}
